01d42c023b75d91ecb3023a9be9bfa7303929dd7
Viswanath1680 <127508141+Viswanath1680@users.noreply.github.com>
2024-01-17 19:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
+    highlight(countEl)
     count += 1
     countEl.textContent = count
 }
 
+function highlight(btn){
+    countEl.className = "";
+    saveEl.className = "";
+    btn.className = "active";
+}
+
 function save() {
+    highlight(saveEl)
     let countStr = count + " - "
     saveEl.textContent += countStr
     count = 0;
